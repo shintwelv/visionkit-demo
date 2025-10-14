7e3cb30d1f70dc439e85fd303a7ebf9a71e43866
허신일 <shintwl@icloud.com>
2025-10-14 10:43:39 +0900

    created select image view

--- FaceDetectionDemo/FaceDetectionDemo/Select Face/SelectFaceView.swift
+++ FaceDetectionDemo/FaceDetectionDemo/Select Face/SelectFaceView.swift
@@ -6,10 +6,57 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct SelectFaceView: View {
+    @State private var pickedItem: PhotosPickerItem?
+    @State private var image: UIImage?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 260)
+            }
+
+            PhotosPicker(selection: $pickedItem,
+                         matching: .images,
+                         photoLibrary: .shared()) {
+                Label("Select Image", systemImage: "photo")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.opacity(0.9))
+                    .foregroundStyle(Color.white)
+                    .clipShape(ButtonBorderShape.roundedRectangle)
+            }
+            .onChange(of: pickedItem) { _, newItem in
+                Task {
+                    if let data = try? await newItem?.loadTransferable(type: Data.self),
+                       let uiImage = UIImage(data: data) {
+                        image = uiImage
+                    }
+                }
+             }
+            
+            if let image {
+                Button {
+                    print("Go button tapped")
+                } label: {
+                    Label("Process", systemImage: "gearshape.fill")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.9))
+                        .foregroundStyle(Color.white)
+                        .clipShape(ButtonBorderShape.roundedRectangle)
+                }
+
+            }
+        }
+        .padding()
     }
 }
 

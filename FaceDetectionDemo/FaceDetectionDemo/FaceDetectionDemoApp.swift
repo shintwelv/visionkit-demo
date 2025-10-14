3f2e8797a13af6d2f7f5a654bb47ad92cabf6471
허신일 <shintwl@icloud.com>
2025-10-14 10:13:42 +0900

    created face detection demo project

---
+++ FaceDetectionDemo/FaceDetectionDemo/FaceDetectionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectionDemoApp.swift
+//  FaceDetectionDemo
+//
+//  Created by ShinIl Heo on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

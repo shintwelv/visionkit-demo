06c30edb4da3a00dc5d29ab0033826b82288ae44
허신일 <shintwl@icloud.com>
2025-10-14 10:43:05 +0900

    created select image view

---
+++ FaceDetectionDemo/FaceDetectionDemo/Select Face/SelectFaceView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectFaceView.swift
+//  FaceDetectionDemo
+//
+//  Created by ShinIl Heo on 10/14/25.
+//
+
+import SwiftUI
+
+struct SelectFaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectFaceView()
+}

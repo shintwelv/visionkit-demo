31265083ba4509186213c61fde0819017fadaf20
허신일 <shintwl@icloud.com>
2025-10-15 11:50:46 +0900

    created comparing two images demo

---
+++ CompareTwoImageDemo/CompareTwoImageDemo/CompareTwoImageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompareTwoImageDemoApp.swift
+//  CompareTwoImageDemo
+//
+//  Created by ShinIl Heo on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CompareTwoImageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompareTwoImageDemo/CompareTwoImageDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CompareTwoImageDemo
+//
+//  Created by ShinIl Heo on 10/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
